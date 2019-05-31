f6075cf0acabc0185b637926862c3ad0302f40b6
Petteri Seppälä <petteri.t.seppala@gmail.com>
2019-05-31 13:01:04 +0300

    new build

---
+++ build/precache-manifest.abd9d436635a7679a41390d1fee09426.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5d04be2137ad91b47e8d1438023530",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84c4769c2296c9ab9856",
+    "url": "/static/css/main.98ad490f.chunk.css"
+  },
+  {
+    "revision": "1a95d5a327b9fa556408",
+    "url": "/static/js/2.9fde0a89.chunk.js"
+  },
+  {
+    "revision": "84c4769c2296c9ab9856",
+    "url": "/static/js/main.0e590590.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
